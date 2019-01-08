de5ef38cce30d5e0ee0b71de164e20443e28a593
Jim O'Donnell <jim@zooniverse.org>
2019-01-08 15:56:52 +0000

    Add a map Fill the viewport with a map and plot the heatmap on top of it.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
+const mapDataURL = '/test-data/overlay_3.csv';
+const center = new google.maps.LatLng(15.231458142,-61.2507115);
+const map = new google.maps.Map(document.getElementById('map'), {
+  center,
+  zoom: 10,
+  mapTypeId: 'satellite'
+});
+
 function parseLine([lat, lng, weight]) {
   const location = new google.maps.LatLng(lat, lng);
   return { location, weight };
 }
+
 function parseMapData(results, file) {
-  const heatMapData = results.data.map(parseLine);
-  console.log(heatMapData);
+  const heatmapData = results.data.map(parseLine);
+  const heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatmapData
+  });
+  heatmap.setMap(map);
 }
-const mapDataURL = '/test-data/overlay_0.csv';
+
 const config = {
   download: true,
+  skipEmptyLines: true,
   complete: parseMapData
 }
 Papa.parse(mapDataURL, config);
