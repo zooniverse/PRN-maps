54b015619d68b23dc44197f934498068a1489f7c
Jim O'Donnell <jim@zooniverse.org>
2019-01-11 15:17:16 +0000

    Add manifests API Add a common API.get() method. Add API.manifests(eventName) to get event manifests.

--- js/api.js
+++ js/api.js
@@ -1,23 +1,22 @@
 const API = {
   host: 'http://localhost.pfe-preview.zooniverse.org:3000',
-  events: function () {
-    return superagent.get(`${API.host}/events`)
+  get: function(path, defaultValue) {
+    return superagent.get(`${API.host}${path}`)
     .then(function (response) {
       return JSON.parse(response.text);
     })
     .catch(function (error) {
       console.error(error);
-      return [];
+      return defaultValue;
     });
   },
+  events: function () {
+    return API.get('/events', []);
+  },
   layers: function (eventName) {
-    return superagent.get(`${API.host}/layers/${eventName}`)
-    .then(function (response) {
-      return JSON.parse(response.text);
-    })
-    .catch(function (error) {
-      console.error(error);
-      return [];
-    });
+    return API.get(`/layers/${eventName}`, []);
+  },
+  manifests: function (eventName) {
+    return API.get(`/manifests/${eventName}`, {});
   }
 }
